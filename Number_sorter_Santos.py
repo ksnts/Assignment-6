6684ec9fb689f077cb728643c3aab07c4fdf2fd7
ksnts <santosjohnkyllem@gmail.com>
2021-11-28 22:58:24 +0800

    Second draft

--- Number_sorter_Santos.py
+++ Number_sorter_Santos.py
@@ -14,10 +14,7 @@ if num1 >= num2 and num1 >=num3 and num1 >=num4:
         else:
             print(num4)
             print(num3)
-            if num3<num4:
-                print(num3)
-            else:
-                print(num4)
+            
     if num3>=num2 and num3>=num4:
         print(num3)
         if num2>=num4:
@@ -26,10 +23,7 @@ if num1 >= num2 and num1 >=num3 and num1 >=num4:
         else:
             print(num4)
             print(num2)
-            if num2<num4:
-                print(num2)
-            else:
-                print(num4)
+           
     if num4>=num3 and num4>=num2:
         print(num4)
         if num3>=num2:
@@ -38,17 +32,94 @@ if num1 >= num2 and num1 >=num3 and num1 >=num4:
         else:
             print(num2)
             print(num3)
-            if num3<num2:
-                print(num3)
-            else:
-                print(num2)
+            
 
-if num2 >= num1 and num2 >=num3 and num2 >=num4:
+elif num2 >= num1 and num2 >=num3 and num2 >=num4:
     print(num2)
+    if num1>=num3 and num1>=num4:
+        print(num1)
+        if num3>=num4:
+            print(num3)
+            print(num4)
+        else:
+            print(num4)
+            print(num3)
+            
+    if num3>=num1 and num3>=num4:
+        print(num3)
+        if num1>=num4:
+            print(num1)
+            print(num4)
+        else:
+            print(num4)
+            print(num1)
+            
+    if num4>=num1 and num4>=num3:
+        print(num4)
+        if num1>=num3:
+            print(num1)
+            print(num3)
+        else:
+            print(num3)
+            print(num1)
+            
 
-if num3 >= num1 and num3 >=num2 and num3 >=num4:
+elif num3 >= num1 and num3 >=num2 and num3 >=num4:
     print(num3)
+    if num1>=num2 and num1>num4:
+        print(num1)
+        if num2>=num4:
+            print(num2)
+            print(num4)
+        else:
+            print(num4)
+            print(num2)
+    if num2>=num1 and num2>=num4:
+        print(num2)
+        if num1>=num4:
+            print(num1)
+            print(num4)
+        else:
+            print(num4)
+            print(num1)
+    if num4>=num1 and num4>=num2:
+        print(num4)
+        if num1>=num2:
+            print(num1)
+            print(num2)
+        else:
+            print(num2)
+            print(num1)
 
-if num4 >= num1 and num4 >=num2 and num4 >=num3:    
+elif num4 >= num1 and num4 >=num2 and num4 >=num3:    
     print(num4)
+    if num1>=num2 and num1>=num3:
+        print(num1)
+        if num2>=num3:
+            print(num2)
+            print(num3) 
+        else:
+            print(num3)
+            print(num2)
+    if num2>=num1 and num2>=num3:
+        print(num2)
+        if num1>=num3:
+            print(num1)
+            print(num3)
+        else:
+            print(num3)
+            print(num1)
+    if num3>=num1 and num3>=num2:
+        print(num3)
+        if num1 >=num2:
+            print(num1)
+            print(num2)
+        else:
+            print(num2)
+            print(num1)
+
+
+
+
+    
  
