3d2c3999ea73d6f89422824421d1bc5f3d11a716
ksnts <santosjohnkyllem@gmail.com>
2021-11-28 20:18:26 +0800

    First Nested if else

--- Number_sorter_Santos.py
+++ Number_sorter_Santos.py
@@ -8,8 +8,47 @@ if num1 >= num2 and num1 >=num3 and num1 >=num4:
     print(num1)
     if num2>=num3 and num2>=num4:
         print(num2)
-    elif num3>=num2 and num3>=num4:
+        if num3>=num4:
+            print(num3)
+            print(num4)
+        else:
+            print(num4)
+            print(num3)
+            if num3<num4:
+                print(num3)
+            else:
+                print(num4)
+    if num3>=num2 and num3>=num4:
         print(num3)
-    elif num4>=num2 and num4>=num3:
+        if num2>=num4:
+            print(num2)
+            print(num4)
+        else:
+            print(num4)
+            print(num2)
+            if num2<num4:
+                print(num2)
+            else:
+                print(num4)
+    if num4>=num3 and num4>=num2:
         print(num4)
+        if num3>=num2:
+            print(num3)
+            print(num2)
+        else:
+            print(num2)
+            print(num3)
+            if num3<num2:
+                print(num3)
+            else:
+                print(num2)
+
+if num2 >= num1 and num2 >=num3 and num2 >=num4:
+    print(num2)
+
+if num3 >= num1 and num3 >=num2 and num3 >=num4:
+    print(num3)
+
+if num4 >= num1 and num4 >=num2 and num4 >=num3:    
+    print(num4)
  
