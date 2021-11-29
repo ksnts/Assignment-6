00f995b2e63ec212108ee23d6bcaa2883c2f9365
ksnts <santosjohnkyllem@gmail.com>
2021-11-29 22:23:12 +0800

    Code complete

--- Random_number_Santos.py
+++ Random_number_Santos.py
@@ -1,25 +1,32 @@
+#random num generator
 from random import randrange
-
-num1 = randrange (0,99)
-num2 = randrange (0,99)
+#variables for scoring
 score = 0
 numItems = 1
 qnum = 1
-
+#question loop
 while numItems <=10:
-    print(f"Question {qnum}: {num1} + {num2} =")
-    userInput = input("Answer: ")
-    qsum = num1 + num2
+    num1 = randrange (0,99)
+    num2 = randrange (0,99)
 
+    print("\n")
+    print(f"Question {qnum}: {num1} + {num2} =")
+    userInput = float(input("Answer: "))
+    qsum = num1 + num2
+    
+#increments for the loop and scoring
     if userInput == qsum:
         score+=1
+        print("Correct !")
     else:
-        score-=1
+        print("Wrong !")
     
     qnum +=1
     numItems +=1
+#total score
+print("\n")
+print(f"Your total score is: {score}/10!")
 
-print("Your score is: ", score)
 
 
 
