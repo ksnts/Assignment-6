d17390059ac7711ddf4cf55aa69a3829eba6deb6
ksnts <santosjohnkyllem@gmail.com>
2021-11-29 21:39:45 +0800

    Code complete

--- Number_sorter_Santos.py
+++ Number_sorter_Santos.py
@@ -1,9 +1,10 @@
+#user inputs the 4 numbers
 num1=float(input("Input first number: "))
 num2=float(input("Input second number: "))
 num3=float(input("Input third number: "))
 num4=float(input("Input fourth number: "))
 
-
+#multiple nested if else conditions to determine order
 if num1 >= num2 and num1 >=num3 and num1 >=num4:
     print(num1)
     if num2>=num3 and num2>=num4:
@@ -15,7 +16,7 @@ if num1 >= num2 and num1 >=num3 and num1 >=num4:
             print(num4)
             print(num3)
             
-    if num3>=num2 and num3>=num4:
+    elif num3>=num2 and num3>=num4:
         print(num3)
         if num2>=num4:
             print(num2)
@@ -24,7 +25,7 @@ if num1 >= num2 and num1 >=num3 and num1 >=num4:
             print(num4)
             print(num2)
            
-    if num4>=num3 and num4>=num2:
+    elif num4>=num3 and num4>=num2:
         print(num4)
         if num3>=num2:
             print(num3)
@@ -45,7 +46,7 @@ elif num2 >= num1 and num2 >=num3 and num2 >=num4:
             print(num4)
             print(num3)
             
-    if num3>=num1 and num3>=num4:
+    elif num3>=num1 and num3>=num4:
         print(num3)
         if num1>=num4:
             print(num1)
@@ -54,7 +55,7 @@ elif num2 >= num1 and num2 >=num3 and num2 >=num4:
             print(num4)
             print(num1)
             
-    if num4>=num1 and num4>=num3:
+    elif num4>=num1 and num4>=num3:
         print(num4)
         if num1>=num3:
             print(num1)
@@ -74,7 +75,7 @@ elif num3 >= num1 and num3 >=num2 and num3 >=num4:
         else:
             print(num4)
             print(num2)
-    if num2>=num1 and num2>=num4:
+    elif num2>=num1 and num2>=num4:
         print(num2)
         if num1>=num4:
             print(num1)
@@ -82,7 +83,7 @@ elif num3 >= num1 and num3 >=num2 and num3 >=num4:
         else:
             print(num4)
             print(num1)
-    if num4>=num1 and num4>=num2:
+    elif num4>=num1 and num4>=num2:
         print(num4)
         if num1>=num2:
             print(num1)
@@ -101,7 +102,7 @@ elif num4 >= num1 and num4 >=num2 and num4 >=num3:
         else:
             print(num3)
             print(num2)
-    if num2>=num1 and num2>=num3:
+    elif num2>=num1 and num2>=num3:
         print(num2)
         if num1>=num3:
             print(num1)
@@ -109,7 +110,7 @@ elif num4 >= num1 and num4 >=num2 and num4 >=num3:
         else:
             print(num3)
             print(num1)
-    if num3>=num1 and num3>=num2:
+    elif num3>=num1 and num3>=num2:
         print(num3)
         if num1 >=num2:
             print(num1)
